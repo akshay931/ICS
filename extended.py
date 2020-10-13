1c8fea04706641ef20186bd936de4253e1c56a35
akshay931 <akshayb3129@gmail.com>
2020-10-13 17:39:11 +0530

    Modified

---
+++ extended.py
@@ -0,0 +1,16 @@
+file1 = open("input",'r')
+file2 = open("output",'w')
+for line in file1:
+    a,b=(int(x) for x in line.split(" "))
+def extended(num1,num2):
+    if(num1==0):
+        return num2,0,1
+    gcd,x,y = extended(num2%num1,num1)
+    x1 = y-(num2//num1)*x
+    y1 = x
+    return gcd,x1,y1
+
+g,a1,b1 = extended(a,b)
+file2.write("GCD("+str(a)+","+str(b)+") = "+str(g)+"\n")
+file2.write("Coefficient of x is "+str(a1)+"\n")
+file2.write("Coefficient of y is "+str(b1))
